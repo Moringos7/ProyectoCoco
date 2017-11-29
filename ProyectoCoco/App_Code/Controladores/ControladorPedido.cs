efba75192640a5b0b9732904d0161302e8a52fb7
MonyGG <33943349+MonyGG@users.noreply.github.com>
2017-11-28 21:13:46 -0600

    Add files via upload

---
+++ ProyectoCoco/App_Code/Controladores/ControladorPedido.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Objects;
+using System.Data.Objects.DataClasses;
+using Modelos;
+
+/// <summary>
+/// Descripción breve de ControladorPedido
+/// </summary>
+public class ControladorPedido
+{
+    CafeteriaPacoEntities BD;
+	public ControladorPedido()
+	{
+        BD = new CafeteriaPacoEntities();
+	}
+    //CREATE PEDIDO
+    public void CrearPedido(Pedido Pedido)
+    {
+        BD.Pedido.Add(Pedido);
+        BD.SaveChanges();
+    }
+    // UPDATE PEDIDO
+    public void ModificarPedido(int PkPedido, Pedido Pedido)
+    {
+        Pedido Temp = new Pedido();
+        Temp = BD.Pedido.Find(PkPedido);
+        Temp.Platillo = Pedido.Platillo;
+        Temp.Orden = Pedido.Orden;
+        BD.SaveChanges();
+    }
+}
