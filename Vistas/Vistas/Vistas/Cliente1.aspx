1d7d0d040fcfd03280b0a6c888081afe8abc52b8
LeonardoMtz98 <33941450+LeonardoMtz98@users.noreply.github.com>
2017-11-29 14:40:44 -0600

    Add files via upload
    Contribucion de Diego Hernandez Elias

---
+++ Vistas/Vistas/Vistas/Cliente1.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Cliente1.aspx.cs" Inherits="Cliente.Cliente1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        #Select1 {
+            height: 29px;
+            width: 131px;
+        }
+        #SelectMesa
+        {
+            position: absolute;
+            right: 2000px;
+        }
+        #Select2 {
+            position: absolute;
+            width: 197px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <p>
+            <asp:Label ID="Label1" runat="server" Font-Size="50px" ForeColor="#A70000" Text="Cliente"></asp:Label>
+        </p>
+        &nbsp;
+        Mesa:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <asp:DropDownList ID="DropDownList2" runat="server">
+      <asp:ListItem>1</asp:ListItem>
+      <asp:ListItem>2</asp:ListItem>
+      <asp:ListItem>3</asp:ListItem>
+      <asp:ListItem>4</asp:ListItem>
+      <asp:ListItem>5</asp:ListItem>
+        </asp:DropDownList>
+        &nbsp;</div>
+        <asp:GridView style="position:absolute; top: 217px; left: 322px;" ID="GridView1" runat="server">
+        </asp:GridView>
+        <p>
+            <asp:DropDownList ID="DropDownList1" runat="server">
+            </asp:DropDownList>
+            <asp:TextBox ID="TextBox1" runat="server">1</asp:TextBox>
+            <asp:Label ID="Label2" runat="server" Text="Numero De orden:"></asp:Label>
+            <asp:Label ID="Label3" runat="server" Text="" style="position:absolute; top: 181px; left: 394px;"></asp:Label>
+            </p>
+            <asp:Button ID="Button1" runat="server" Height="40px" OnClick="Button1_Click" Text="Cargar" Width="106px" UseSubmitBehavior="False" />
+            <asp:ListBox ID="ListBox1" runat="server" Height="114px" Width="194px"></asp:ListBox>
+        <p>
+            <asp:Button ID="Button2" runat="server" Height="73px" Text="Ordenar" Width="111px" OnClick="Button2_Click" />
+        </p>
+    </form>
+    </body>
+</html>
---
+++ Vistas/Vistas/Vistas/Cliente1.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Cliente.ServiceReference1;
+
+namespace Cliente
+{
+    public partial class Cliente1 : System.Web.UI.Page
+    {
+        ServiceReference1.WebServiceSoapClient servicio = new WebServiceSoapClient();
+        List<ModeloPedido> Pedidos = new List<ModeloPedido>();
+        int Orden;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DropDownList1.DataSource = servicio.DevolverTablaPlatillo().ToList<ModeloPlatillo>().Select(p =>p.Nombre);
+            DropDownList1.DataBind();
+            Orden = servicio.DevolverTablaOrden().Select(o => o.PkOrden).Max()+1;
+            GridView1.DataSource = Pedidos;
+            GridView1.DataBind();
+            Label3.Text = Convert.ToString(Orden);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int cantidad;
+            cantidad=Convert.ToInt32(TextBox1.Text);
+            ListBox1.Items.Add(DropDownList1.Text + "  "+ TextBox1.Text);
+
+
+            /*for (int y = 0; y < cantidad; y++)
+            {
+                ModeloPedido Temporal = new ModeloPedido();
+                int Platillo = servicio.DevolverTablaPlatillo().Where(P => P.Nombre == DropDownList1.Text).Select(P => P.PkPlatillo).FirstOrDefault();
+                Temporal.Platillo = Platillo;
+                Temporal.Orden = Orden;
+                Pedidos.Add(Temporal);
+            }*/
+            //GridView1.DataBind();
+            
+            if (servicio.DevolverTablaOrden().Select(O => O.PkOrden).Max() < Orden)
+            {
+                DateTime Fecha = new DateTime();
+                Fecha = DateTime.Now;
+                servicio.CrearOrden(Convert.ToInt32(DropDownList2.Text), Fecha);                
+            }
+            int Platillo = servicio.DevolverTablaPlatillo().Where(P => P.Nombre == DropDownList1.Text).Select(P => P.PkPlatillo).FirstOrDefault();
+            servicio.CrearPedido(Platillo, Orden);
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Page.Response.Redirect(Page.Request.Url.ToString(), true);
+        }
+    }
+}
